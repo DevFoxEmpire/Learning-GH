01a92f4ed59e8c3c15bd17bb758506d8f6b0f99d
OlliGN <Olga199411@gmail.com>
2022-11-19 19:27:01 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Olha Hladush on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
